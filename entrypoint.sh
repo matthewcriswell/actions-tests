87c63d5053d8813c7c59402572f71708a7369046
Matt Criswell <llewsircttam@gmail.com>
2021-11-01 13:21:05 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ docker run --rm alpine sh -c "apk add --no-cache curl; curl https://binary.miran
 apk update
 apk add curl
 
+#add to PATH
+PATH=$PATH:$(pwd)
+
 #install kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
