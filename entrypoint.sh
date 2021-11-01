4a2bbbd777ae752c988b6f4c8443cb3e4f31951e
Matt Criswell <llewsircttam@gmail.com>
2021-11-01 12:03:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo "Hello $1"
 time=$(date)
-apk add curl kubectl
+apk update
+apk add kubectl
 docker run --rm alpine sh -c "apk add --no-cache curl; curl https://binary.mirantis.com"
 
 echo "::set-output name=time::$time"
