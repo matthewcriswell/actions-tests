89d1d0fdef93483f0aecfa9f225f096113a529b9
Matt Criswell <llewsircttam@gmail.com>
2021-11-01 12:18:58 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ time=$(date)
 # test access
 docker run --rm alpine sh -c "apk add --no-cache curl; curl https://binary.mirantis.com"
 
+#get curl
+apk update
+apk add curl
+
 #install kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
