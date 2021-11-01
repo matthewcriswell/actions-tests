f24bcdf752445a845c75d7507ee5f0dc1e56600f
Matt Criswell <llewsircttam@gmail.com>
2021-11-01 12:21:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,11 @@ apk add curl
 #install kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
+./kind create cluster
 ./kind get clusters
 
 #write a bogus file outside of workdir
-echo "secrets" >> /usr/local/sensitive_data.txt
+#echo "secrets" >> /usr/local/sensitive_data.txt
 
 # usually for debugging
 #sleep 100000
