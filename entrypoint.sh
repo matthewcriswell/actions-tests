52c62d90ab50b9560a85a34f29f58dae223cd2e5
Matt Criswell <llewsircttam@gmail.com>
2021-11-01 12:11:30 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,19 @@
 
 echo "Hello $1"
 time=$(date)
-apk update
-apk search kubectl
+
+# test access
 docker run --rm alpine sh -c "apk add --no-cache curl; curl https://binary.mirantis.com"
 
-sleep 100000
+#install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+./kind get clusters
+
+#write a bogus file outside of workdir
+echo "secrets" >> /usr/local/sensitive_data.txt
+
+# usually for debugging
+#sleep 100000
 
 echo "::set-output name=time::$time"
